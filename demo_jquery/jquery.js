926f01a45d422603041e864a1afc84e1127d853e
pixelcore3000 <115512280+ci1f-class@users.noreply.github.com>
2022-10-31 21:41:18 -0400

    jquery demo

---
+++ demo_jquery/jquery.js
@@ -0,0 +1,11 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[hello earthlings]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
